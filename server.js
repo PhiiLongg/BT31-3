8ef45149a14abb33804d6eebb8706dae60d2408b
PhiiLongg <philong372003@gmail.com>
2025-03-31 15:46:33 +0700

    htmlhtml

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const menuRoutes = require('./routes/menu');
+const cors = require('cors'); // Thêm cors
 require('dotenv').config();
 
 const app = express();
 
 // Middleware
+app.use(cors()); // Thêm dòng này để kích hoạt CORS
 app.use(express.json());
 
 // Connect to database
